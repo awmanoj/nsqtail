59c061376f54f68c86d9ba573dae8d841b45d6ef
Manoj Awasthi <awasthi.manoj@gmail.com>
2020-10-20 03:20:11 +0700

    simplify html

--- html/tmpl.index.go
+++ html/tmpl.index.go
@@ -10,34 +10,13 @@ var IndexHTML = `
 		<head>
 			<title>NSQ Tail Topics via {{ .NSQLookupdAddress }}</title>
 			<style>
-				  body {
-					width: 60%;
-				  }
-				
-				  h1 {
-					font-family: "Geneva";
-					background-color: #C0C4E4;
-				  }
-				
-				
-				  p {
-					font-family: "Geneva";
-				  } 
-				
-				  a {
-					font-family: "Geneva"
-				  }
-				
-				  #footer {
-					background-color: #DFE1F1;
-				  }
 			</style>
 		</head>
 		<body>
 			<h1>Topics</h1>
 			<ul>
 				{{range .Topics}}
-					<li><a href="/nsqtail/{{.}}">{{.}}</a></li>
+					<a href="/nsqtail/{{.}}">{{.}}</a>
 				{{end}}
 			</ul>
 		</body>
--- html/tmpl.tail.go
+++ html/tmpl.tail.go
@@ -12,34 +12,14 @@ var TailHTML = `
 		<head>
 			<title>NSQ Tail for {{.Topic}} via {{ .NSQLookupdAddress }}</title>
 			<style>
-				  body {
-					width: 60%;
-				  }
-				
-				  h1 {
-					font-family: "Geneva";
-					background-color: #C0C4E4;
-				  }
-				
-				
-				  p {
-					font-family: "Geneva";
-				  } 
-				
-				  a {
-					font-family: "Geneva"
-				  }
-				
-				  #footer {
-					background-color: #DFE1F1;
-				  }
 			</style>
 		</head>
 		<body>
 			<h1>Last {{ .MessageCount }} messages for topic {{ .Topic }}</h1>
 			<ul>
 				{{range .Messages}}
-					<li>{{.}}</li>
+					{{.}}
+					<hr/>
 				{{end}}
 			</ul>
 		</body>
